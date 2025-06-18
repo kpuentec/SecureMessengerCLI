b73773433142863c619057fd11442c8ff50dbefd
kpuentec <kcortez1130@gmail.com>
2025-06-18 15:01:02 -0400

    Save before merge

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ import threading
 import secrets
 from hashlib import sha256
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from colorama import Fore, Style, init
 
 HOST = "192.168.1.162"
 PORT = 9999
-USERNAME = "User"
-OTHER_USER = "Friend"
-
 
 def verify():
-    print("To begin, make sure HOST and PORT is properly configured in main.py")
+    print("\nTo begin, make sure HOST and PORT is properly configured in main.py")
+    print("To exit the chat simpliy type in /quit.")
     choice = input("Enter 1 to continue: ").strip()
 
     if choice == "1":
-        client, key = chat()
-        threading.Thread(target=send, args=(client, key)).start()
-        threading.Thread(target=receive, args=(client, key)).start()
+        client, key, name, their_name, color, their_color = chat()
+        threading.Thread(target=send, args=(client, key, name, color)).start()
+        threading.Thread(target=receive, args=(client, key, their_name, their_color)).start()
 
         while True:
             pass
+
     else:
         print("Invalid Input. Exitting program.\n")
         main()
@@ -30,6 +30,7 @@ def chat():
     print("1. Host (start a server) ")
     print("2. Client (connect to a host) ")
     choice = input("Enter your choice: ").strip()
+    username = input("Enter your username: ").strip()
 
     g = 14
     p = 292192301
@@ -37,6 +38,8 @@ def chat():
     public_key = pow(g, private_key, p)
 
     if choice == "1":
+        color = Fore.GREEN
+        their_color = Fore.BLUE
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((HOST, PORT))
         server.listen()
@@ -45,34 +48,55 @@ def chat():
         client.sendall(str(public_key).encode())
 
         # recieve other public key
-        their_public = int(client.recv(2048).decode())
+        client.sendall(username.encode())
+        their_public = int(client.recv(1024).decode())
+        their_name = client.recv(1024).decode()
 
     elif choice == "2":
+        color = Fore.BLUE
+        their_color = Fore.GREEN
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((HOST, PORT)) 
-        their_public = int(client.recv(2048).decode())
+        their_public = int(client.recv(1024).decode())
+        their_name = client.recv(1024).decode()
+
         client.sendall(str(public_key).encode())
+        client.sendall(username.encode())
     else:
         print("Invalid Input. Exitting program.")
         main()
 
     shared_secret = pow(their_public, private_key, p)
     key = sha256(str(shared_secret).encode()).digest()
+    print(f"{Fore.YELLOW}Connection established! You can start chatting now.{Style.RESET_ALL}")
+    return client, key, username, their_name, color, their_color
 
-    return client, key
-
-def send(c, k):
+def send(c, k, name, color):
     while True:
         message = input("")
+        # message = input(color + name + Style.RESET_ALL + " -- ")
+        if message.lower() == "/quit":
+            c.send(encrypt("__quit__", k))
+            print("You ended the chat.")
+            c.close()
+            break
         encrypted = encrypt(message, k)
         c.send(encrypted)
-        print(f"{USERNAME}: " + message)
+        print('\033[F\033[K', end='')
+        print()
+        print(color + name + Style.RESET_ALL + " -- " + message)
 
-def receive(c, k):
+def receive(c, k, their_name, their_color):
     while True:
         data = c.recv(2048)
-        decrypted = decrypt(data, k)
-        print(f"{OTHER_USER}: " + decrypted)
+        if data:
+            decrypted = decrypt(data, k)
+            if decrypted == "__quit__":
+                print(f"\n Chat has been ended.")
+                c.close()
+                break
+            print()
+            print(their_color + their_name + Style.RESET_ALL + " -- " + decrypted)
 
 def encrypt(message, key):
     aesgcm = AESGCM(key)
@@ -87,46 +111,24 @@ def decrypt(data, key):
     plaintext = aesgcm.decrypt(nonce, ciphertext, None)
     return plaintext.decode()
 
-
-
 def main():
         print("\n")
         print("*===== Secure Messenger CLI =====*\n")
         print("Securely exchange messages\n")
 
         print("1. Begin Connection")
-        print("2. Modify Usernames")
-        print("3. Quit")
+        print("2. Quit")
         choice = input("Choose an option: ").strip()
 
         if choice == "1":
             verify()
         elif choice == "2":
-            print("1. Modify Username")
-            print("2. Modify Friend Username")
-            print("3. Main Menu")
-            choice = input("Choose an option: ").strip()
-
-            if choice == "1":
-                USERNAME = input("Choose an option: ").strip()
-                print("Username Changed.")
-                main()
-            elif choice == "2":
-                OTHER_USER = input("Choose an option: ").strip()
-                print("Username Changed.")
-                main()
-            elif choice == "3":
-                main()
-            else:
-                print("Invalid choice. Try again.\n")
-                main()
-        elif choice == "3":
             exit()
         else:
             print("Invalid choice. Try again.\n")
+            main()
 
 
 if __name__ == "__main__":
+    init(autoreset=True)
     main()
-
-# python3 main.py
